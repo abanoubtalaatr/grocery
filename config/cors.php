598f7e371ea1ab83b7a00c4f2ef22caafa7245c7
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2026-01-16 14:07:58 +0200

    wip

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,33 @@
 <?php
 
 return [
+
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['/api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://127.0.0.1:5500', 'http://localhost:3000'], // your frontend
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'https://r8-react-cure-1-git-final-result-mohamedmatar001s-projects.vercel.app',
+    ],
+
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
@@ -13,5 +35,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => true
+
 ];
