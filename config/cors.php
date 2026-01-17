4abb003ec77dc00b5663268aca210abfd653bd37
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2026-01-17 15:13:00 +0200

    wip

--- config/cors.php
+++ config/cors.php
@@ -24,7 +24,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'https://r8-react-cure-1-git-final-result-mohamedmatar001s-projects.vercel.app',
+        'https://grocery-plus-22.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
